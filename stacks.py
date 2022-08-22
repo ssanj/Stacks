ae0ed0a9f4fbb58b03613bceb070bd11ff5dba3c
sanjiv sahayam <sanjsmailbox@gmail.com>
2022-08-23 09:35:17 +1000

    Save stack name and load from stack name

--- stacks.py
+++ stacks.py
@@ -40,22 +40,30 @@ class StacksSaveCommand(sublime_plugin.WindowCommand):
       if 'folder' in window.extract_variables():
         project_dir: str = window.extract_variables()['folder']
 
-        # views: List[ViewFileName] = [ViewFileName(v, FileName(v.file_name())) for v in window.views() if v.file_name() and not v.is_scratch() and not v.is_dirty()]
-        # views_to_save: List[str] = list(map(lambda v: v.file_name.value, views))
-        views_to_save = _get_window_state(window)
-
-        # TODO: Merge with existing values
-        json_content: str = json.dumps({ _get_stack_name(window) : views_to_save})
-
-        with open(f"{project_dir}/{_stack_file_name}", "w") as file:
-           file.write(json_content)
-
-        # TODO: Do we need to move this option to config?
-        _close_open_views(window)
+        window.show_input_panel(
+          caption = "Stack name",
+          on_done = lambda sn: self.on_stack_name(project_dir, window, sn),
+          initial_text = "",
+          on_change = None,
+          on_cancel = None
+        )
       else:
         sublime.message_dialog("Could not find project directory")
     else:
       sublime.message_dialog("No active window found")
+
+  def on_stack_name(self, project_dir: str, window: sublime.Window, stack_name: str) -> None:
+    # TODO: Merge with existing values
+    # TODO: if name is already taken prompt for overwrite confirmation?
+    views_to_save = _get_window_state(window)
+    json_content: str = json.dumps({ stack_name : views_to_save})
+
+    with open(f"{project_dir}/{_stack_file_name}", "w") as file:
+       file.write(json_content)
+
+    # TODO: Do we need to move this option to config?
+    _close_open_views(window)
+
 
 class StacksOpenCommand(sublime_plugin.WindowCommand):
 
@@ -66,40 +74,54 @@ class StacksOpenCommand(sublime_plugin.WindowCommand):
       if 'folder' in window.extract_variables():
         project_dir: str = window.extract_variables()['folder']
 
-        saved_file = f"{project_dir}/{_stack_file_name}"
+        stack_file = f"{project_dir}/{_stack_file_name}"
 
-        if os.path.exists(saved_file):
-          with open(saved_file, "r") as file:
+        if os.path.exists(stack_file):
+          with open(stack_file, "r") as file:
             try:
               loaded_stacks: Dict[str, Any] = json.loads(file.read())
             except json.decoder.JSONDecodeError:
-              sublime.message_dialog(f"Could not decode {saved_file}.\nConsider deleting it and resaving it or make it a valid json file.")
+              sublime.message_dialog(f"Could not decode {stack_file}.\nConsider deleting it and resaving it or make it a valid json file.")
               return
 
-            stack_name = _get_stack_name(window)
-            has_stack_name_in_file = stack_name in loaded_stacks
-            if stack_name:
-              if has_stack_name_in_file:
-                window_state: Dict[str, Any] = loaded_stacks[stack_name]
+            items: List[str] = [key for key in loaded_stacks.keys()]
 
-                _close_open_views(window)
-                window.set_layout(window_state['layout'])
-
-                # TODO make this safer or use a regex
-                key_groups: List[int] = [int(key.split("group")[1]) for key in window_state.keys() if key.startswith("group")]
-
-                for group in key_groups:
-                  views_in_group = window_state[f"group{group}"]
-                  for v in views_in_group:
-                    # TODO: Check if the file still exists
-                    window.open_file(fname = v, group = group)
-              else:
-                sublime.message_dialog(f"Could not find stack named:\n{stack_name}\nin:\n{saved_file}")
+            window.show_quick_panel(
+              items = items,
+              placeholder = "Which stack would you like to load?",
+              on_select = lambda index: self.on_stack_load(stack_file, window, loaded_stacks, items, index)
+            )
 
         else:
-          sublime.message_dialog(f"Could not find saved file:\n{saved_file}.\nPlease try saving a stack first.")
+          sublime.message_dialog(f"Could not find saved file:\n{stack_file}.\nPlease try saving a stack first.")
       else:
         sublime.message_dialog("Could not find project directory")
 
     else:
       sublime.message_dialog("No active window found")
+
+  def on_stack_load(self, stack_file: str, window: sublime.Window, loaded_stacks: Dict[str, Any], stack_names: List[str], stack_index: int) -> None:
+    if stack_index < 0 or stack_index > len(stack_names):
+      return
+
+    stack_name = stack_names[stack_index]
+
+    # TODO: Validate stack_name
+    has_stack_name_in_file = stack_name in loaded_stacks
+    if has_stack_name_in_file:
+      window_state: Dict[str, Any] = loaded_stacks[stack_name]
+
+      _close_open_views(window)
+      window.set_layout(window_state['layout'])
+
+      # TODO make this safer or use a regex
+      key_groups: List[int] = [int(key.split("group")[1]) for key in window_state.keys() if key.startswith("group")]
+
+      for group in key_groups:
+        views_in_group = window_state[f"group{group}"]
+        for v in views_in_group:
+          # TODO: Check if the file still exists
+          window.open_file(fname = v, group = group)
+    else:
+      sublime.message_dialog(f"Could not find stack named:\n{stack_name}\nin:\n{stack_file}")
+
