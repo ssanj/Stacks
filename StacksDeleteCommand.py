fc6f8646b9a33b0bde300139e32ddbf5f5425e32
sanjiv sahayam <sanjsmailbox@gmail.com>
2022-08-24 14:22:57 +1000

    Update delete to base loader

--- StacksDeleteCommand.py
+++ StacksDeleteCommand.py
@@ -4,50 +4,37 @@ from typing import Optional, List, Any, Dict
 import json
 import os
 from Stacks.components.Common import _get_window_state, _stack_file_name, _close_open_views, _loaded_stack_name_settings_key
-from Stacks.StacksCommand import StacksCommand
+from Stacks.StacksLoaderCommand import StacksLoaderCommand
+from Stacks.components.FileUtils import SaveError, save_stack_file
+from Stacks.components.Files import StackFileName
+from Stacks.components.ResultTypes import Either
 
-class StacksDeleteCommand(StacksCommand):
+class StacksDeleteCommand(StacksLoaderCommand):
 
-  def on_run(self, window: sublime.Window, stack_file: str):
-    if os.path.exists(stack_file):
-      with open(stack_file, "r") as file:
-        try:
-          loaded_stacks: Dict[str, Any] = json.loads(file.read())
-        except json.decoder.JSONDecodeError:
-          sublime.message_dialog(f"Could not decode {stack_file}.\nConsider deleting it and resaving it or make it a valid json file.")
-          return
+  def loader_message(self) -> str:
+    return "Which stack would you like to delete?"
 
-        items: List[str] = [key for key in loaded_stacks.keys()]
 
-        window.show_quick_panel(
-          items = items,
-          placeholder = "Which stack would you like to delete?",
-          on_select = lambda index: self.on_stack_delete(stack_file, window, loaded_stacks, items, index)
-        )
-    else:
-      sublime.message_dialog(f"Could not find saved file:\n{stack_file}.\nPlease try saving a stack first.")
-
-  def on_stack_delete(self, stack_file: str, window: sublime.Window, loaded_stacks: Dict[str, Any], stack_names: List[str], stack_index: int) -> None:
-    if stack_index < 0 or stack_index > len(stack_names):
+  def on_stack_loaded(self, stack_file: StackFileName, window: sublime.Window, loaded_stacks: Dict[str, Any], stack_names: List[str], stack_name_index: int) -> None:
+    if stack_name_index < 0 or stack_name_index > len(stack_names):
       return
 
-    stack_to_delete = stack_names[stack_index]
+    stack_to_delete = stack_names[stack_name_index]
 
     loaded_stacks.pop(stack_to_delete, None)
     updated_stack_content = json.dumps(loaded_stacks)
 
-    try:
-      with open(stack_file, "w") as file:
-        file.write(updated_stack_content)
-    except Exception as e:
-      sublime.message_dialog(f"Could not save stack.\nError:\n{str(e)}")
-      return
+    save_result: Either[SaveError, None] = save_stack_file(stack_file, updated_stack_content)
+    if save_result.has_value():
+      # get the currently loaded stack name
+      current_stack_name = window.settings().get(_loaded_stack_name_settings_key)
 
-    # get the currently loaded stack name
-    current_stack_name = window.settings().get(_loaded_stack_name_settings_key)
+      # if it matches the deleted stack name, then remove it from settings
+      if current_stack_name and current_stack_name == stack_to_delete:
+        window.settings().erase(_loaded_stack_name_settings_key)
 
-    # if it matches the deleted stack name, then remove it from settings
-    if current_stack_name and current_stack_name == stack_to_delete:
-      window.settings().erase(_loaded_stack_name_settings_key)
+      sublime.message_dialog(f"Deleted {stack_to_delete}")
+    else:
+      error: SaveError = save_result.error()
+      sublime.message_dialog(f"Could not save stack.\nError:\n{error}")
 
-    sublime.message_dialog(f"Deleted {stack_to_delete}")
