bd9fcaebccbc7d68c1e32c4a37976a07f15d8add
sanjiv sahayam <sanjsmailbox@gmail.com>
2022-08-26 09:59:19 +1000

    MVP Preview on load

--- StacksLoaderCommand.py
+++ StacksLoaderCommand.py
@@ -1,7 +1,7 @@
 import sublime
 from Stacks.StacksCommand import StacksCommand
 from abc import abstractmethod
-from typing import NamedTuple, Optional, List, Any, Dict
+from typing import NamedTuple, Optional, List, Any, Dict, Tuple
 from Stacks.components.FileUtils import LoadError, load_stack_file
 from Stacks.components.ResultTypes import Either, RightEither, LeftEither
 from Stacks.components.Files import StackFileName
@@ -22,6 +22,7 @@ class StacksLoaderCommand(StacksCommand):
       window.show_quick_panel(
         items = items,
         placeholder = self.loader_message(),
+        on_highlight = lambda index: self.on_stack_highlight(window, logger, stack_file, loaded_stacks, items, index),
         on_select = lambda index: self.on_stack_loaded(window, logger, stack_file, loaded_stacks, items, index)
       )
     else:
@@ -33,6 +34,34 @@ class StacksLoaderCommand(StacksCommand):
         sublime.message_dialog(f"Could not decode {stack_file}.\nConsider deleting it and resaving it or make it a valid json file.")
       else:
         sublime.message_dialog(f"An unexpected error occurred: {error}")
+
+  def on_stack_highlight(self, window: sublime.Window, logger: Logger, stack_file: StackFileName, loaded_stacks: Dict[str, Any], stack_names: List[str], stack_name_index: int) -> None:
+    if stack_name_index < 0 or stack_name_index > len(stack_names):
+      return
+
+    selected_stack_name = stack_names[stack_name_index]
+    stack = loaded_stacks.get(selected_stack_name)
+    if not stack:
+      return
+
+    stack_to_preview: Dict[str, Any] = stack
+
+    group_names: List[str] = [key for key in stack_to_preview.keys() if key.startswith("group")]
+
+    group_files: List[Tuple[str, List[str]]] = [(g, stack_to_preview[g]) for g in group_names]
+
+    view = window.active_view()
+    if view:
+      content = ""
+      for gf in group_files:
+        group_str: str = gf[0]
+        files: List[str] = gf[1]
+        files_str: str = "<br/>".join(files)
+        content = f"{content}<br/>{group_str}:<br/>{files_str}"
+
+      layout_extent: Tuple[float, float] = view.layout_extent()
+      view.show_popup(content, max_width=int(layout_extent[0]), max_height=int(layout_extent[1]))
+
 
   def on_stack_loaded(self, window: sublime.Window, logger: Logger, stack_file: StackFileName, loaded_stacks: Dict[str, Any], stack_names: List[str], stack_name_index: int) -> None:
     if stack_name_index < 0 or stack_name_index > len(stack_names):
